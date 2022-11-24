c62da13d68b1672b2bba39204cf38ee835a4edc6
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-11-24 22:18:09 +0800

    footer added

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const MainLayout = () => {
@@ -7,6 +8,7 @@ const MainLayout = () => {
     <div className="min-h-screen max-w-screen-xl mx-auto">
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer className="m-5"></Footer>
     </div>
   );
 };
